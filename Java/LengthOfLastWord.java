a52c666237dc6394f1eb5be4060dfebc44302186
SpicyChickenFLY <xixiayuchao@gmail.com>
2019-06-07 09:15:24 +0800

    Create new problem

---
+++ Java/LengthOfLastWord.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+    }
+}
+
+public class lengthOfLastWord {
+    public static void main(String[] args) {
+        String inputs[] = {
+            "hello world",
+            "ni hao",
+            "en ",
+            " ",
+            ""
+        };
+        for (String str: inputs) {
+            System.out.println("input: " + str);
+        }
+
+    }
+}
+
+
