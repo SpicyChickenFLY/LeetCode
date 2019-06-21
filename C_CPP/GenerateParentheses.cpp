69888c7af52c8014c5815e360d0970a40450d283
SpicyChickenFLY <xixiayuchao@gmail.com>
2019-06-21 15:59:40 +0800

    finsh new problem

--- C_CPP/GenerateParentheses.cpp
+++ C_CPP/GenerateParentheses.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <unordered_map>
 #include <string>
 using namespace std;
@@ -17,11 +18,27 @@ public:
             result.push_back("()");
         else if (record.find(n) == record.end()) {
             vector<string> subResult = recursion(n - 1);
-            for (int i = 0; i < subResult.size; i++)
+            set<string> temp;
+            for (int i = 0; i < subResult.size(); i++)
             {
-                result.push_back("()" + subResult.at(i));
-                result.push_back("(" + subResult.at(i) + ")");
+                temp.insert("()" + subResult.at(i));
+                temp.insert("(" + subResult.at(i) + ")");
+                temp.insert(subResult.at(i) + "()");
             }
+            
+            if (n >= 4) {
+                for (int i = 2; i <= n / 2; i++) {
+                    vector<string> subResult1 = recursion(i);
+                    vector<string> subResult2 = recursion(n - i);
+                    for (int j = 0; j < subResult1.size(); j++)
+                        for (int k = 0; k < subResult2.size(); k++)
+                        {
+                            temp.insert(subResult1.at(j) + subResult2.at(k));
+                            temp.insert(subResult2.at(k) + subResult1.at(j));
+                        }
+                }
+            }
+            result.assign(temp.begin(), temp.end());
             record.insert({{n, result}});
         }
         else
@@ -39,10 +56,11 @@ public:
 int main(int argc, char const *argv[])
 {
     Solution *s = new Solution();
-    for (int i = 1; i < 10; i++)
+    for (int i = 1; i < 5; i++)
     {
+        cout << i << endl;
         vector<string> result = s->generateParenthesis(i);
-        for (int j = 0; j < result.size; j++)
+        for (int j = 0; j < result.size(); j++)
         {
             cout << result.at(j) << endl;
         }
