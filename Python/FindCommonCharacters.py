8e5e14c2b183b25fda645e796ea244a502a7a01b
SpicyChickenFLY <xixiayuchao@gmail.com>
2019-07-19 20:37:08 +0800

    update unfinished problem

--- Python/FindCommonCharacters.py
+++ Python/FindCommonCharacters.py
@@ -5,8 +5,24 @@ class Solution:
         result = []
         for char in A[0]:
             result.append(char)
-        for i in range():
-
+        result.sort()
+        for i in range(1, len(A)):
+            p = 0
+            q = 0
+            new_list = []
+            for char in A[i]:
+                new_list.append(char)
+            new_list.sort()
+            while (p < len(result) and q < len(new_list)):
+                if result[p] == new_list[q]:
+                    p += 1
+                    q += 1
+                elif result[p] > new_list[q]:
+                    q += 1
+                else: 
+                    result.remove(result[p])
+            if q == len(new_list):
+                result = result[:p]
         return result
 
 if __name__ == "__main__":
