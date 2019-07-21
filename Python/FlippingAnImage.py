1a0f73571a4e7eeffa720cad895300f5f8f8776e
SpicyChickenFLY <xixiayuchao@gmail.com>
2019-07-21 11:05:52 +0800

    modify performance of solution

--- Python/FlippingAnImage.py
+++ Python/FlippingAnImage.py
@@ -1,5 +1,5 @@
 class Solution:
-    # Runtime: 74.01%  Memory Usage: 5.27%
+    # Runtime: 10.41%  Memory Usage: 5.27%
     def flipAndInvertImage(self, A):
         return [[1 if s==0 else 0 for s in l[::-1]] for l in A]
 
