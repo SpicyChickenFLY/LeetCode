c7559c6fa5f05266cb510f3dec23cf67bf11634d
SpicyChickenFLY <xixiayuchao@gmail.com>
2019-10-25 23:14:43 +0800

    unfinished new problem

---
+++ Java/CombinationSumIII.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        
+    }
+}
+
+public class CombinationSumIII {
+    Solution s = new Solution();
+    for (int k = 1; k < 4; k++) {
+        for (int n = 3; n < )
+    }
+}
